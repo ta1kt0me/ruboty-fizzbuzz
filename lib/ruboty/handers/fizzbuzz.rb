705c67feb7b765c06e167a330d3fb1cc7cd6d8e1
ta1kt0me <p.wadachi@gmail.com>
2015-05-14 23:04:47 +0900

    Add on method in handler

--- lib/ruboty/handers/fizzbuzz.rb
+++ lib/ruboty/handers/fizzbuzz.rb
@@ -3,6 +3,7 @@ require 'ruboty/Fizzbuzz/actions/fizzbuzz'
 module Ruboty
   module Handler
     class Fizzbuzz < Base
+      on /fizzbuzz (?<number>.*?)\z/, name: 'fizzbuzz', description: 'output fizzbuzz result'
     end
   end
 end
