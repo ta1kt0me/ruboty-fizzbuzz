9397bc4cbf9dbee1e7ff1a380fef3f5241b5faf3
ta1kt0me <p.wadachi@gmail.com>
2015-05-14 23:08:05 +0900

    Implement fizzbuzz action in handler

--- lib/ruboty/handers/fizzbuzz.rb
+++ lib/ruboty/handers/fizzbuzz.rb
@@ -4,6 +4,10 @@ module Ruboty
   module Handler
     class Fizzbuzz < Base
       on /fizzbuzz (?<number>.*?)\z/, name: 'fizzbuzz', description: 'output fizzbuzz result'
+
+      def fizzbuzz(message)
+        Ruboty::Fizzbuzz::Actions::Fizzbuzz.new(message).call
+      end
     end
   end
 end
